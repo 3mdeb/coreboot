2abf54cdfa679f3dca35ac48e8b70a2757a813ed
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-09-23 17:10:58 +0200

    src/arch/ppc64/include/arch/io.h: remove obsolete statements
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/arch/ppc64/include/arch/io.h
+++ src/arch/ppc64/include/arch/io.h
@@ -10,32 +10,30 @@
 #define LPCHC_IO_SPACE 0xD0010000
 #define LPC_BASE_ADDR (MMIO_GROUP0_CHIP0_LPC_BASE_ADDR + LPCHC_IO_SPACE - LPC_ADDR_START)
 
+/* Enforce In-order Execution of I/O */
 static inline void eieio(void)
 {
-    asm volatile("eieio" ::: "memory");
+	asm volatile("eieio" ::: "memory");
 }
 
 static inline void outb(uint8_t value, uint16_t port)
 {
 	uint8_t *l_ptr = (uint8_t *)(LPC_BASE_ADDR + port);
-	const uint8_t *i_ptr = (const uint8_t *)&value;
-	*l_ptr = *i_ptr;
+	*l_ptr = value;
 	eieio();
 }
 
 static inline void outw(uint16_t value, uint16_t port)
 {
 	uint16_t *l_ptr = (uint16_t *)(LPC_BASE_ADDR + port);
-	const uint16_t *i_ptr = (const uint16_t *)&value;
-	*l_ptr = *i_ptr;
+	*l_ptr = value;
 	eieio();
 }
 
 static inline void outl(uint32_t value, uint16_t port)
 {
 	uint32_t *l_ptr = (uint32_t *)(LPC_BASE_ADDR + port);
-	const uint32_t *i_ptr = (const uint32_t *)&value;
-	*l_ptr = *i_ptr;
+	*l_ptr = value;
 	eieio();
 }
 
@@ -43,8 +41,7 @@ static inline uint8_t inb(uint16_t port)
 {
 	uint8_t buffer;
 	uint8_t *l_ptr = (uint8_t *)(LPC_BASE_ADDR + port);
-	uint8_t *o_ptr = (uint8_t *)&buffer;
-	*o_ptr = *l_ptr;
+	buffer = *l_ptr;
 	eieio();
 	return buffer;
 }
@@ -53,8 +50,7 @@ static inline uint16_t inw(uint16_t port)
 {
 	uint16_t buffer;
 	uint16_t *l_ptr = (uint16_t *)(LPC_BASE_ADDR + port);
-	uint16_t *o_ptr = (uint16_t *)&buffer;
-	*o_ptr = *l_ptr;
+	buffer = *l_ptr;
 	eieio();
 	return buffer;
 }
@@ -63,8 +59,7 @@ static inline uint32_t inl(uint16_t port)
 {
 	uint32_t buffer;
 	uint32_t *l_ptr = (uint32_t *)(LPC_BASE_ADDR + port);
-	uint32_t *o_ptr = (uint32_t *)&buffer;
-	*o_ptr = *l_ptr;
+	buffer = *l_ptr;
 	eieio();
 	return buffer;
 }

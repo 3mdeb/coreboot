c1afb3cbfe0c28b2f296090b0473f0f2317a204b
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-09-25 12:21:05 +0200

    mainboard/talos-2/bootblock.c: do not repeat what lib/bootblock does
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/mainboard/raptor-cs/talos-2/bootblock.c
+++ src/mainboard/raptor-cs/talos-2/bootblock.c
@@ -7,25 +7,9 @@
 #include <console/uart.h>
 #include <bootblock_common.h>
 
-void bootblock_main(void);
-
-
-static void early_config_superio(void)
+void bootblock_mainboard_early_init(void)
 {
 	const pnp_devfn_t serial_dev = PNP_DEV(0x2e, AST2400_SUART1);
 	aspeed_enable_uart_pin(serial_dev);
 	aspeed_enable_serial(serial_dev, CONFIG_TTYS0_BASE);
-}
-
-/* The qemu part of all this is very, very non-hardware like.
- * So it gets its own bootblock.
- */
-void bootblock_mainboard_early_init(void)
-{
-	early_config_superio();
-	if (CONFIG(BOOTBLOCK_CONSOLE)) {
-		console_init();
-	}
-
-	run_romstage();
 }

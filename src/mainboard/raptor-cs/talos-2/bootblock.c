4dd45b75ea2dde0007532eab55976eb0d9559360
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-09-25 16:00:33 +0200

    src/mainboard/raptor-cs/talos-2/bootblock.c: initialize SoL instead of UART1
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/raptor-cs/talos-2/bootblock.c
+++ src/mainboard/raptor-cs/talos-2/bootblock.c
@@ -9,7 +9,56 @@
 
 void bootblock_mainboard_early_init(void)
 {
-	const pnp_devfn_t serial_dev = PNP_DEV(0x2e, AST2400_SUART1);
-	aspeed_enable_uart_pin(serial_dev);
-	aspeed_enable_serial(serial_dev, CONFIG_TTYS0_BASE);
+	struct config_data vuart_init[] = {
+		/* Activate VUART */
+		[Step1] = {
+			.type = SIO,
+			.base = 0,
+			.reg = ACT_REG,
+			.and = 0,
+			.or = ACTIVATE_VALUE
+		},
+		/* Configure IRQ level as low */
+		[Step2] = {
+			.type = MEM,
+			.base = ASPEED_VUART_BASE,
+			.reg = 0x20,
+			.and = ~2,
+			.or = 0
+		},
+		/* Set the SerIRQ IRQ 4 */
+		[Step3] = {
+			.type = MEM,
+			.base = ASPEED_VUART_BASE,
+			.reg = 0x24,
+			.and = ~0xf0,
+			.or = (4 << 4)
+		},
+		[Step4] = {
+			.type = MEM,
+			.base = ASPEED_VUART_BASE,
+			.reg = 0x24,
+			.and = ~0xf0,
+			.or = (4 << 4)
+		},
+		/* Configure base address low */
+		[Step5] = {
+			.type = MEM,
+			.base = ASPEED_VUART_BASE,
+			.reg = 0x28,
+			.and = 0,
+			.or = CONFIG_TTYS0_BASE & 0xff
+		},
+		/* Configure base address high */
+		[Step6] = {
+			.type = MEM,
+			.base = ASPEED_VUART_BASE,
+			.reg = 0x2c,
+			.and = 0,
+			.or = (CONFIG_TTYS0_BASE >> 8) & 0xff
+		},
+	};
+
+	const pnp_devfn_t serial_dev = PNP_DEV(0x2e, AST2400_ILPC2AHB);
+	aspeed_early_config(serial_dev, vuart_init, ARRAY_SIZE(vuart_init));
 }

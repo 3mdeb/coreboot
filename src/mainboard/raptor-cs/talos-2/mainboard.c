73a126691b0180120763b5ef9eae2357aa47ca26
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2021-01-25 08:13:04 +0100

    src/arch/ppc64/include/arch/io.h: Move report_istep()

--- src/mainboard/raptor-cs/talos-2/mainboard.c
+++ src/mainboard/raptor-cs/talos-2/mainboard.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <arch/io.h>
 #include <console/console.h>
 #include <device/device.h>
 #include <cbmem.h>
@@ -20,9 +19,3 @@ static void mainboard_enable(struct device *dev)
 struct chip_operations mainboard_ops = {
 	.enable_dev = mainboard_enable,
 };
-
-static inline void report_istep(uint8_t step, uint8_t substep)
-{
-	outb(step, 0x81);
-	outb(substep, 0x82);
-}

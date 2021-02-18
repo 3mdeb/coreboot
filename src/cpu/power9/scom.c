f774fb7b2797747c50f7d47f4d95cac2afa9c428
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-02-18 12:55:30 +0100

    cpu/power9: fix directory structure and timer build fault
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/cpu/power9/scom.c
+++ src/cpu/power9/scom.c
@@ -26,6 +26,7 @@ void write_scom_indirect(uint64_t reg_address, uint64_t value)
 	addr = reg_address & 0x7FFFFFFF;
 	data = reg_address & XSCOM_ADDR_IND_ADDR;
 	data |= value & XSCOM_ADDR_IND_DATA;
+<<<<<<< HEAD
 
 	write_scom_direct(addr, data);
 
@@ -39,12 +40,76 @@ void write_scom_indirect(uint64_t reg_address, uint64_t value)
 			       reg_address, value, data);
 		}
 		// TODO: delay?
+=======
+	printk(BIOS_EMERG, "----------------\n");
+	printk(BIOS_EMERG, "SCOM WRITE\n\n");
+	/*
+	 * Reading 0x90013 here doesn't make sense. This register reports errors in
+	 * the most recent XSCOM access, and is cleared as soon as a new access is
+	 * attempted, meaning we can only see that reading 0x90013 was successful.
+	 * It is possible to read this register through 'pdbg' from BMC which
+	 * doesn't use XSCOM, in which case 0x90013 isn't polluted.
+	 */
+	printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+	printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+		read_hmer(),
+		read_hmer() & SPR_HMER_XSCOM_FAIL,
+		read_hmer() & SPR_HMER_XSCOM_DONE,
+		read_hmer() & SPR_HMER_XSCOM_STATUS);
+	printk(BIOS_EMERG, "WRITING addr = %llX, data = %llX\n", addr, data);
+	for(unsigned int i = 0; i < 0xFFFF; ++i) {
+		asm volatile("nop" :::"memory");
+	}
+	write_scom_direct(addr, data);
+	printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+		read_hmer(),
+		read_hmer() & SPR_HMER_XSCOM_FAIL,
+		read_hmer() & SPR_HMER_XSCOM_DONE,
+		read_hmer() & SPR_HMER_XSCOM_STATUS);
+	printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+	for(int retries = 0; retries < XSCOM_IND_MAX_RETRIES; ++retries) {
+		printk(BIOS_EMERG, "READING addr = %llX\n", addr);
+		for(unsigned int i = 0; i < 0xFFFF; ++i) {
+			asm volatile("nop" :::"memory");
+		}
+		data = read_scom_direct(addr);
+		printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+			read_hmer(),
+			read_hmer() & SPR_HMER_XSCOM_FAIL,
+			read_hmer() & SPR_HMER_XSCOM_DONE,
+			read_hmer() & SPR_HMER_XSCOM_STATUS);
+		printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+		printk(BIOS_EMERG, "READ data = %llX\n", data);
+		if((data & XSCOM_DATA_IND_COMPLETE) && ((data & XSCOM_DATA_IND_ERR) == 0)) {
+			printk(BIOS_EMERG, "SUCCESS\n");
+			printk(BIOS_EMERG, "----------------\n");
+			return;
+		}
+		else if(data & XSCOM_DATA_IND_COMPLETE) {
+			printk(BIOS_EMERG, "FINISHED WITH ERROR\n");
+			printk(BIOS_EMERG, "----------------\n");
+			return;
+		}
+>>>>>>> cpu/power9: fix directory structure and timer build fault
 	}
 }
 
 uint64_t read_scom_indirect(uint64_t reg_address)
 {
 	uint64_t addr;
+<<<<<<< HEAD
 	uint64_t data;
 	addr = reg_address & 0x7FFFFFFF;
 	data = XSCOM_DATA_IND_READ | (reg_address & XSCOM_ADDR_IND_ADDR);
@@ -61,6 +126,68 @@ uint64_t read_scom_indirect(uint64_t reg_address)
 			       reg_address, data);
 		}
 		// TODO: delay?
+=======
+	uint64_t data = -1;
+	addr = reg_address & 0x7FFFFFFF;
+	data = XSCOM_DATA_IND_READ | (reg_address & XSCOM_ADDR_IND_ADDR);
+	printk(BIOS_EMERG, "----------------\n");
+	printk(BIOS_EMERG, "SCOM READ\n\n");
+	printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+	printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+		read_hmer(),
+		read_hmer() & SPR_HMER_XSCOM_FAIL,
+		read_hmer() & SPR_HMER_XSCOM_DONE,
+		read_hmer() & SPR_HMER_XSCOM_STATUS);
+	printk(BIOS_EMERG, "WRITING addr = %llX, data = %llX\n", addr, data);
+	for(unsigned int i = 0; i < 0xFFFF; ++i) {
+		asm volatile("nop" :::"memory");
+	}
+	write_scom_direct(addr, data);
+	printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+		read_hmer(),
+		read_hmer() & SPR_HMER_XSCOM_FAIL,
+		read_hmer() & SPR_HMER_XSCOM_DONE,
+		read_hmer() & SPR_HMER_XSCOM_STATUS);
+	printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+	for(int retries = 0; retries < XSCOM_IND_MAX_RETRIES; ++retries) {
+		printk(BIOS_EMERG, "READING addr = %llX\n", addr);
+		for(unsigned int i = 0; i < 0xFFFF; ++i) {
+			asm volatile("nop" :::"memory");
+		}
+		data = read_scom_direct(addr);
+		printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+			read_hmer(),
+			read_hmer() & SPR_HMER_XSCOM_FAIL,
+			read_hmer() & SPR_HMER_XSCOM_DONE,
+			read_hmer() & SPR_HMER_XSCOM_STATUS);
+		printk(BIOS_EMERG, "*0x90010 = %llX,\n*0x90011 = %llX,\n*0x90012 = %llX,\n*0x90013 = %llX\n",
+		read_scom_direct(0x90010),
+		read_scom_direct(0x90011),
+		read_scom_direct(0x90012),
+		read_scom_direct(0x90013));
+		printk(BIOS_EMERG, "READ data = %llX\n", data);
+		printk(BIOS_EMERG, "#hmer = %llX,\n#fail = %llX,\n#done = %llX,\n#status = %llX\n",
+			read_hmer(),
+			read_hmer() & SPR_HMER_XSCOM_FAIL,
+			read_hmer() & SPR_HMER_XSCOM_DONE,
+			read_hmer() & SPR_HMER_XSCOM_STATUS);
+		if((data & XSCOM_DATA_IND_COMPLETE) && ((data & XSCOM_DATA_IND_ERR) == 0)) {
+			printk(BIOS_EMERG, "SUCCESS\n");
+			printk(BIOS_EMERG, "----------------\n");
+		}
+		else if(data & XSCOM_DATA_IND_COMPLETE) {
+			printk(BIOS_EMERG, "FINISHED WITH ERROR\n");
+			printk(BIOS_EMERG, "----------------\n");
+		}
+>>>>>>> cpu/power9: fix directory structure and timer build fault
 	}
 
 	return data & XSCOM_DATA_IND_DATA;

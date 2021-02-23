1fa6cc8d3a4a3d77e489e4d4c81c96c4cb44cf30
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-02-23 12:33:32 +0100

    mainboard/talos-2: read SPD and fill DIMM attributes
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/mainboard/raptor-cs/talos-2/romstage.c
+++ src/mainboard/raptor-cs/talos-2/romstage.c
@@ -3,12 +3,30 @@
 #include <console/console.h>
 #include <cpu/power/vpd.h>
 #include <program_loading.h>
+#include <device/smbus_host.h>
+#include <device/dram/ddr4.h>
+#include <lib.h>	// hexdump
+#include <spd_bin.h>
 
 void main(void)
 {
-	console_init();
+	int i;
+	dimm_attr dimm[CONFIG_DIMM_MAX];
 
+	console_init();
 	vpd_pnor_main();
+
+	struct spd_block blk = {
+		.addr_map = { DIMM0, DIMM1, DIMM2, DIMM3, DIMM4, DIMM5, DIMM6, DIMM7},
+	};
+
+	get_spd_smbus(&blk);
+	hexdump(blk.spd_array[2], blk.len);
+	dump_spd_info(&blk);
+
+	for (i = 0; i < CONFIG_DIMM_MAX; i++) {
+		spd_decode_ddr4(&dimm[i], blk.spd_array[i]);
+	}
 
 	run_ramstage();
 }

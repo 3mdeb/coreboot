e9588af363be8b548f8196305b18f24e3031256a
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2021-03-30 14:56:48 +0200

    src/soc/ibm/power9/istep_14_1.c: Implement configuration of FARB0Q, MCA_ACTION1 with correct port

--- src/soc/ibm/power9/istep_14_1.c
+++ src/soc/ibm/power9/istep_14_1.c
@@ -89,18 +89,13 @@ void memDiag(void){
     return;
 }
 
-#define DSM0Q_REG 0x000000000701090A // PORT 0
-#define DSM0Q_REG 0x000000000701094A // PORT 1
-#define DSM0Q_REG 0x000000000701098A // PORT 2
-#define DSM0Q_REG 0x00000000070109CA // PORT 3
+#define DSM0Q_REG   0x0701090A
+#define MBA_FARB0Q  0x07010913
+#define MCA_ACTION1 0x07010A07
+#define MCA_MBACALFIRQ 0x7010940
+#define MCA_FIR     0x7010A00
 
-#define MBA_FARB0Q 0x0000000007010913 // PORT 0
-#define MBA_FARB0Q 0x0000000007010953 // PORT 1
-#define MBA_FARB0Q 0x0000000007010993 // PORT 2
-#define MBA_FARB0Q 0x00000000070109D3 // PORT 3
-
-template<>
-fapi2::ReturnCode after_memdiags<mss::mc_type::NIMBUS>( const fapi2::Target<fapi2::TARGET_TYPE_MCBIST>& i_target )
+fapi2::ReturnCode after_memdiags(const fapi2::Target<fapi2::TARGET_TYPE_MCBIST>& i_target)
 {
     fapi2::ReturnCode l_rc1, l_rc2;
     fapi2::buffer<uint64_t> dsm0_buffer;
@@ -112,34 +107,17 @@ fapi2::ReturnCode after_memdiags<mss::mc_type::NIMBUS>( const fapi2::Target<fapi
     // Broadcast mode workaround for UEs causing out of sync
     mss::workarounds::mcbist::broadcast_out_of_sync(i_target, mss::ON);
 
-    for (const auto& p : mss::find_targets<TARGET_TYPE_MCA>(i_target))
+    for(unsigned int port = 0; port < 4; ++port)
     {
         fir::reg<MCA_FIR> l_ecc64_fir_reg(p, l_rc1);
         fir::reg<MCA_MBACALFIRQ> l_cal_fir_reg(p, l_rc2);
         uint64_t rcd_protect_time = 0;
-        const auto l_chip_target = mss::find_target<fapi2::TARGET_TYPE_PROC_CHIP>(i_target);
-        fapi2::buffer<uint64_t> l_data;
 
-        // Read out the wr_done and rd_tag delays and find min
-        // and set the RCD Protect Time to this value
-        // ----
-        // mss::read_dsm0q_register(p, dsm0_buffer);
-        mss::getScom(p, DSM0Q_REG, dsm0_buffer);
-        // ----
-        // mss::get_wrdone_delay(dsm0_buffer, wr_done_delay);
-        dsm0_buffer.template extractToRight<24, 6>(wr_done_delay);
-        // mss::get_rdtag_delay(dsm0_buffer, rd_tag_delay);
+        dsm0_buffer = scom_read(DSM0Q_REG | (port << PORT_OFFSET));
+        wr_done_delay = (dsm0_buffer & 0xFC00000000) >> 34;
+        rd_tag_delay = (i_data & 0xFC00000) >> 22;
         rcd_protect_time = min(wr_done_delay, rd_tag_delay);
-
-        // ----
-        // mss::change_rcd_protect_time(p, rcd_protect_time);
-        // get farb0q register
-        mss::getScom(p, MBA_FARB0Q, l_data);
-        // set rcd protect time
-        l_data.template insertFromRight<48, 6>(rcd_protect_time);
-        // set farb0q register
-        mss::putScom(p, MBA_FARB0Q, l_data);
-        // ----
+        scom_and_or(MBA_FARB0Q | (port << PORT_OFFSET), ~0xFC00, (rcd_protect_time & 0x3F) << 10);
 
         l_ecc64_fir_reg.checkstop<MCA_FIR_MAINLINE_AUE>()
           .recoverable_error<MCA_FIR_MAINLINE_UE>()
@@ -158,7 +136,7 @@ fapi2::ReturnCode after_memdiags<mss::mc_type::NIMBUS>( const fapi2::Target<fapi
         // If MNFG FLAG Threshhold is enabled skip IUE unflagging
         mss::mnfg_flags(l_mnfg_buffer);
 
-        if (!(l_mnfg_buffer.getBit<63>()))
+        if (!l_mnfg_buffer.getBit<63>())
         {
             l_ecc64_fir_reg.recoverable_error<MCA_FIR_MAINTENANCE_IUE>();
         }
@@ -166,26 +144,8 @@ fapi2::ReturnCode after_memdiags<mss::mc_type::NIMBUS>( const fapi2::Target<fapi
         l_ecc64_fir_reg.write();
         l_cal_fir_reg.write();
 
-        // Change Maint AUE and IAUE to checkstop without unmasking
-        // Normal setup modifies masked bits in addition to setting checkstop
-        // This causes issues if error has occured, manually scoming to avoid this
-        mss::getScom(p, MCA_ACTION1, l_aue_buffer);
-        l_aue_buffer.clearBit<MCA_FIR_MAINTENANCE_AUE>();
-        l_aue_buffer.clearBit<MCA_FIR_MAINTENANCE_IAUE>();
-        mss::putScom(p, MCA_ACTION1, l_aue_buffer);
-
-        // Note: We also want to include the following setup RCD recovery and port fail
-        // ----
-        // mss::change_port_fail_disable(p, mss::LOW);
-        mss::getScom(p, MBA_FARB0Q, l_data);
-        l_data.writeBit<TT::PORT_FAIL_DISABLE>(mss::LOW);
-        mss::putScom(p, MBA_FARB0Q, l_data);
-        // ----
-        // mss::change_rcd_recovery_disable(p, mss::LOW);
-        mss::getScom(p, MBA_FARB0Q, l_data);
-        l_data.writeBit<TT::RCD_RECOVERY_DISABLE>(mss::LOW);
-        mss::putScom(p, MBA_FARB0Q, l_data);
-        // ----
+        scom_and(MCA_ACTION1 | (port << PORT_OFFSET), ~0x48000000)
+        scom_and(MBA_FARB0Q | (port << PORT_OFFSET), ~0x240);
     }
 }
 

3733c6fb10c144f8d68e56bdb649516352534497
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2021-04-28 07:50:00 +0200

    src/soc/ibm/power9/istep_14_2.c: Implement throttleSync()

--- src/soc/ibm/power9/istep_14_2.c
+++ src/soc/ibm/power9/istep_14_2.c
@@ -3,7 +3,7 @@
 void istep_14_2(void)
 {
 	thermalInit();
-	run_proc_throttle_sync();
+	throttleSync();
 }
 
 void thermalInit(void)
@@ -18,76 +18,15 @@ void thermalInit(void)
 		scom_and_for_chiplet(MCSTarget, MCS_MCMODE0, PPC_BIT(21));
 	}
 }
-
-void run_proc_throttle_sync(void)
+void throttleSync(chiplet_id_t MCSTarget)
 {
-	TARGETING::TargetHandleList l_procChips;
-	getAllChips(l_procChips, TARGETING::TYPE_PROC);
-	for(const auto & l_procChip: l_procChips)
+	for(size_t MCSIndex = 0; MCSIndex < LEN(MCSTargets); ++MCSIndex)
 	{
-		throttleSync();
+		progMCSMode0(MCSTargets[MCSIndex]);
 	}
+	progMaster(MCSTargets[0]);
 }
 
-template< fapi2::TargetType T>
-fapi2::ReturnCode throttleSync(chiplet_id_t MCSTarget)
-{
-	// MAX_MC_SIDES_PER_PROC = 2
-	mcSideInfo_t<T> l_mcSide[MAX_MC_SIDES_PER_PROC];
-	uint8_t l_sideNum = 0;
-	uint8_t l_pos = 0;
-	uint8_t l_numMasterProgrammed = 0;
-
-	// Initialization
-	// MAX_MC_SIDES_PER_PROC = 2
-	for (l_sideNum = 0; l_sideNum < MAX_MC_SIDES_PER_PROC; l_sideNum++)
-	{
-		l_mcSide[l_sideNum].masterMcFound = false;
-	}
-
-	// ---------------------------------------------------------------------
-	// 1. Pick the first MCS/MI with DIMMS as potential master
-	//    for both MC sides (MC01/MC23)
-	// ---------------------------------------------------------------------
-	for(size_t MCSIndex = 0; MCSIndex < LEN(MCSTargets); ++MCSIndex) {
-	{
-		chiplet_id_t MCSTarget = MCSTargets[MCSIndex];
-		// This MCS or MI has DIMMs attached, find out which MC side it
-		// belongs to:
-		//    l_sideNum = 0 --> MC01
-		//                1 --> MC23
-		l_mcSide[0].masterMcFound = true;
-		l_mcSide[0].masterMc = l_mc;
-		progMCMODE0(MCSTarget, MCSTargets);
-	}
-
-	// --------------------------------------------------------------
-	// 2. Program the master MCS or MI
-	// --------------------------------------------------------------
-	// MAX_MC_SIDES_PER_PROC = 2
-	for (l_sideNum = 0; l_sideNum < MAX_MC_SIDES_PER_PROC; l_sideNum++)
-	{
-		// If there is a potential master MCS or MI found for this side
-		if (l_mcSide[l_sideNum].masterMcFound == true)
-		{
-			// No master MCS or MI programmed for either side yet,
-			// go ahead and program this MCS or MI as master.
-			if (l_numMasterProgrammed == 0)
-			{
-				progMaster(l_mcSide[l_sideNum].masterMc);
-				l_numMasterProgrammed++;
-			}
-		}
-	}
-}
-
-// template <fapi2::TargetType T>
-// struct mcSideInfo_t
-// {
-//     bool masterMcFound = false;
-//     fapi2::Target<T> masterMc;   // Master MC for this MC side
-// };
-//
 void progMaster(chiplet_id_t MCSTTarget)
 {
 	scom_and_for_chiplet(MCSTTarget, MCS_MCSYNC, ~PPC_BIT(MCS_MCSYNC_SYNC_GO_CH0));
@@ -95,7 +34,7 @@ void progMaster(chiplet_id_t MCSTTarget)
 	scom_and_for_chiplet(MCSTTarget, MCS_MCSYNC, ~PPC_BIT(MBA_REFRESH_SYNC_BIT));
 }
 
-void progMCMODE0(chiplet_id_t MCSTTarget, chiplet_id_t MCSTTargets[])
+void progMCSMode0(chiplet_id_t MCSTTarget)
 {
 	uint64_t l_scomMask =
 		PPC_BIT(MCS_MCMODE0_DISABLE_MC_SYNC)

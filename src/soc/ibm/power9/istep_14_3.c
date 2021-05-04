9f48fd49621847bb19089e0e8fa2a95b26eac0b2
Igor Bagnucki <igor.bagnucki@3mdeb.com>
2021-05-04 09:53:14 +0200

    Fix compilation errors

--- src/soc/ibm/power9/istep_14_3.c
+++ src/soc/ibm/power9/istep_14_3.c
@@ -1,10 +1,12 @@
 #include <cpu/power/istep_14.h>
 
-static void p9_pcie_config()
+static void p9_pcie_config(void)
 {
 	for(size_t PECIndex = 0; PECIndex < PEC_PER_PROC; ++PECIndex)
 	{
-		scom_and_for_chiplet(pec_ids[PECIndex], P9N2_PEC_ADDREXTMASK_REG, ~PPC_BITMASK(0, 6))
+		scom_and_for_chiplet(
+			pec_ids[PECIndex], P9N2_PEC_ADDREXTMASK_REG,
+			~PPC_BITMASK(0, 6));
 		scom_or_for_chiplet(
 			pec_ids[PECIndex], PEC_PBCQHWCFG_REG,
 			PPC_BIT(3) | PPC_BIT(7) | PPC_BIT(11)
@@ -17,13 +19,13 @@ static void p9_pcie_config()
 			| PPC_BIT(PEC_PBCQHWCFG_REG_PE_DISABLE_RD_VG)
 			| PPC_BIT(PEC_PBCQHWCFG_REG_PE_DISABLE_RD_SCOPE_GROUP)
 			| PPC_BIT(PEC_PBCQHWCFG_REG_PE_DISABLE_TCE_SCOPE_GROUP)
-			| PPC_BIT(PEC_PBCQHWCFG_REG_PE_DISABLE_TCE_VG))
+			| PPC_BIT(PEC_PBCQHWCFG_REG_PE_DISABLE_TCE_VG));
 		scom_or_for_chiplet(
 			pec_ids[PECIndex], PEC_NESTTRC_REG,
 			PPC_BIT(0) | PPC_BIT(3));
 		write_scom_for_chiplet(
 			pec_ids[PECIndex], PEC_PBAIBHWCFG_REG,
-			0xe00000 | PPC_BIT(PEC_PBAIBHWCFG_REG_PE_PCIE_CLK_TRACE_EN))
+			0xe00000 | PPC_BIT(PEC_PBAIBHWCFG_REG_PE_PCIE_CLK_TRACE_EN));
 	}
 }
 

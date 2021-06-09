6542930b29770afca1b1a738e7126fcc3d9afa58
Krystian Hebel <krystian.hebel@3mdeb.com>
2021-06-09 11:06:46 +0200

    soc/power9/chip.c: fixes after review
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Change-Id: I4a0e5ee969fb5be58479ca394e95165b24ecbcf6

--- src/soc/ibm/power9/chip.c
+++ src/soc/ibm/power9/chip.c
@@ -26,17 +26,17 @@ static int dt_platform_fixup(struct device_tree_fixup *fixup,
 #define BASE_MASK	PPC_BITMASK(24,47)
 #define BASE_SHIFT	(63 - 47)
 
-/* Value in registers is in 4GB units, ram_resource() expects kilobytes. */
-#define TO_KB(x)	((x) << 22)
+/* Values in registers are in 4GB units, ram_resource() expects kilobytes. */
+#define CONVERT_4GB_TO_KB(x)	((x) << 22)
 
 static inline unsigned long base_k(uint64_t reg)
 {
-	return TO_KB((reg & BASE_MASK) >> BASE_SHIFT);
+	return CONVERT_4GB_TO_KB((reg & BASE_MASK) >> BASE_SHIFT);
 }
 
 static inline unsigned long size_k(uint64_t reg)
 {
-	return TO_KB(((reg & SIZE_MASK) >> SIZE_SHIFT) + 1);
+	return CONVERT_4GB_TO_KB(((reg & SIZE_MASK) >> SIZE_SHIFT) + 1);
 }
 
 static void enable_soc_dev(struct device *dev)
@@ -48,10 +48,12 @@ static void enable_soc_dev(struct device *dev)
 		chiplet_id_t nest = mcs_to_nest[mcs_ids[mcs_i]];
 
 		/* These registers are undocumented, see istep 14.5. */
+		/* MCS_MCFGP */
 		reg = read_scom_for_chiplet(nest, 0x0501080A);
 		if (reg & PPC_BIT(0))
 			ram_resource(dev, idx++, base_k(reg), size_k(reg));
 
+		/* MCS_MCFGPM */
 		reg = read_scom_for_chiplet(nest, 0x0501080C);
 		if (reg & PPC_BIT(0))
 			ram_resource(dev, idx++, base_k(reg), size_k(reg));
